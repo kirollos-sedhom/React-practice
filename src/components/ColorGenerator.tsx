03fd62b76208f73091844051d1ed35ca25da3c7f
carlo <kirollosedhom@gmail.com>
2025-03-21 16:58:02 +0200

    finish color randomizer

--- src/components/ColorGenerator.tsx
+++ src/components/ColorGenerator.tsx
@@ -3,6 +3,18 @@ import React from "react";
 const ColorGenerator = () => {
   const [backGroundColor, setBackGroundColor] = React.useState("bg-stone-50");
   const [colorSystem, setColorSystem] = React.useState("hex");
+  const [showColorName, setShowColorName] = React.useState(false); // makes sure its not apparent on first run
+  // const firstRender = React.useRef(true);
+  // React.useEffect(() => {
+  //   if (firstRender.current) {
+  //     firstRender.current = false;
+
+  //     return;
+  //   }
+  //   setShowColorName(true);
+  // }, []);
+  // console.log(firstRender);
+  // console.log(showColorName);
   function generateHexColor() {
     const choices = [
       0,
@@ -27,6 +39,9 @@ const ColorGenerator = () => {
       const randomIndex = Math.floor(Math.random() * choices.length);
       hexCode += choices[randomIndex];
     }
+
+    setShowColorName(true);
+
     setColorSystem("hex");
     setBackGroundColor(hexCode);
   }
@@ -35,6 +50,8 @@ const ColorGenerator = () => {
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     const bg_color = `${red},${green},${blue}`;
+
+    setShowColorName(true);
 
     setColorSystem("rgb");
     setBackGroundColor(bg_color);
@@ -45,8 +62,11 @@ const ColorGenerator = () => {
       ? { backgroundColor: `#${backGroundColor}` }
       : { backgroundColor: `RGB(${backGroundColor})` };
   return (
-    <div style={pageStyle} className="h-screen">
-      <div className="color_generate_options flex justify-center gap-4 py-4">
+    <div
+      style={pageStyle}
+      className="h-screen relative flex flex-col items-center  "
+    >
+      <div className="color_generate_options w-full flex justify-center gap-4 p-4">
         <button
           onClick={generateHexColor}
           className="bg-slate-200 py-1 px-2 rounded-sm cursor-pointer hover:bg-blue-200 focus:bg-blue-400"
@@ -60,6 +80,13 @@ const ColorGenerator = () => {
           RGB color
         </button>
       </div>
+      {showColorName && (
+        <p className="text-center absolute top-1/2 w-full text-center text-6xl text-white">
+          {colorSystem === "rgb"
+            ? `RGB(${backGroundColor})`
+            : `#${backGroundColor.toLocaleUpperCase()}`}
+        </p>
+      )}
     </div>
   );
 };
