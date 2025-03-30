cbccbf963116cfad545b372e5386d154b69b47a2
carlo <kirollosedhom@gmail.com>
2025-03-30 21:00:47 +0200

    apply state management

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 
 type CatResponse = {
-  // todo: get types of the response
+  height: number;
+  id: string;
+  url: string;
+  width: number;
 };
 const ImageSlider = () => {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState<CatResponse[] | null>(null);
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
-
+  // console.log(data[0]);
+  console.log(currentIndex);
+  console.log("data length:", data?.length);
   React.useEffect(() => {
     getData();
   }, []);
 
   async function getData() {
     setLoading(true);
-    const url = "https://cataas.com/cat?json=true";
+    const apiKey = import.meta.env.VITE_CAT_API_KEY;
+    const url = `https://api.thecatapi.com/v1/images/search?limit=5&breed_ids=beng&api_key=${apiKey}`;
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -25,16 +32,40 @@ const ImageSlider = () => {
     } catch (error: unknown) {
       if (error instanceof Error) console.error(error.message);
     } finally {
+      console.log("data is");
       console.log(data);
     }
   }
   return data ? (
     <div>
-      <img src={data.url} />
+      <button onClick={decreaseIndex}>prev</button>
+      <img
+        className="w-[500px] h-[500px] object-cover  transition-all  duration-300 ease-in-out"
+        src={data[currentIndex].url}
+      />
+      <button onClick={increaseIndex}>next</button>
     </div>
   ) : (
     <div>ImageSlider</div>
   );
+
+  function increaseIndex() {
+    if (data) {
+      if (currentIndex === data?.length - 1) setCurrentIndex(0);
+      else {
+        setCurrentIndex(currentIndex + 1);
+      }
+    }
+  }
+  function decreaseIndex() {
+    if (data) {
+      if (currentIndex === 0) {
+        setCurrentIndex(data.length - 1);
+      } else {
+        setCurrentIndex(currentIndex - 1);
+      }
+    }
+  }
 };
 
 export default ImageSlider;
