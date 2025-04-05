b015d7107821d9b859a6667f422cc3c2aa1845b2
carlo <kirollosedhom@gmail.com>
2025-04-05 12:01:59 +0200

    adjust image sizes

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -59,14 +59,19 @@ const ImageSlider = () => {
           onClick={decreaseIndex}
           className="text-4xl mx-2 cursor-pointer"
         />
-        <div className="viewport overflow-hidden w-[500px]">
+        <div className="viewport overflow-hidden w-[500px] border-2">
           <div
             className="track flex transition-transform duration-500 ease-in-out"
             style={{ transform: `translateX(-${currentIndex * 100}%)` }}
           >
             {data.map((item, index) => {
               return (
-                <img src={item.url} alt="a unique cat image" key={index} />
+                <img
+                  className="w-[500px] h-[500px] object-cover flex-shrink-0 border-2"
+                  src={item.url}
+                  alt="a unique cat image"
+                  key={item.id}
+                />
               );
             })}
           </div>
