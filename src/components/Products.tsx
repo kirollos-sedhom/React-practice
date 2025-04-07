0ab31f3abd32ec270cfe6abe1fcea0d616b7b7d7
carlo <kirollosedhom@gmail.com>
2025-04-07 11:59:51 +0200

    adjust product design

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -26,7 +26,7 @@ const Products = () => {
   async function getData() {
     setIsLoading(true);
     try {
-      const url = "https://dummyjson.com/products?limit=50";
+      const url = "https://dummyjson.com/products?limit=10";
       const response = await fetch(url);
 
       if (!response.ok) {
@@ -46,15 +46,23 @@ const Products = () => {
   }
   return data.length > 0 ? (
     <div>
-      {data.map((item, index) => {
-        return (
-          <img
-            className="w-[300px]"
-            src={item.images[0]}
-            alt={item.description}
-          />
-        );
-      })}
+      <div className="items flex flex-wrap">
+        {data.map((item, index) => {
+          return (
+            <Product
+              images={item.images}
+              description={item.description}
+              title={item.title}
+              price={item.price}
+              id={item.id}
+            />
+          );
+        })}
+      </div>
+
+      <button className="bg-zinc-300 p-2 rounded-md font-medium mx-auto my-6 block cursor-pointer">
+        load more
+      </button>
     </div>
   ) : (
     <div>Products</div>
@@ -62,3 +70,22 @@ const Products = () => {
 };
 
 export default Products;
+
+function Product(props: Product) {
+  return (
+    <div className="w-7/10 sm:w-[300px] md:w-[320px] mx-auto">
+      <img
+        src={props.images[0]}
+        className="max-h-64 mx-auto"
+        alt={props.description}
+      />
+      <div className="p-4">
+        <p>{props.title}</p>
+        <p>{props.price}</p>
+        <button className="bg-yellow-300 px-2 py-1 rounded-md my-2 cursor-pointer hover:bg-yellow-400">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+}
