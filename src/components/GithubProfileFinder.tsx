0d95378c3ffb393b4ad61804e019e8e38c8527c3
carlo <kirollosedhom@gmail.com>
2025-04-27 12:46:16 +0300

    get user by name

--- src/components/GithubProfileFinder.tsx
+++ src/components/GithubProfileFinder.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 /*
 you should use github public api to fetch user details
@@ -9,6 +9,14 @@ get name , profile link , join date, public repos, followers, following
 
 const GithubProfileFinder = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <div className="flex flex-col items-center">
       <div className="search_menu flex gap-4 mt-4">
@@ -32,8 +40,31 @@ const GithubProfileFinder = () => {
   function handleSearch() {
     if (inputRef.current) {
       console.log(inputRef.current.value);
+
+      getData(inputRef.current.value);
     } else {
       console.log("please write something");
+    }
+  }
+
+  async function getData(userName: string) {
+    setIsLoading(true);
+    const github_token = import.meta.env.VITE_GITHUB_TOKEN;
+    const url = `https://api.github.com/users/${userName}`;
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`response status ${response.status}`);
+      }
+
+      const json = await response.json();
+      setData(json);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
     }
   }
 };
